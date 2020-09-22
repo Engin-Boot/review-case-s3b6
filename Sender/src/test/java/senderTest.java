eb8c0b67238ef720c044b3fe3c494869814ce219
harisht26698 <harish.t@philips.com>
2020-09-22 11:58:14 +0530

    unit test added for processing data class

--- Sender/src/test/java/senderTest.java
+++ Sender/src/test/java/senderTest.java
@@ -1,9 +1,19 @@
+import com.philips.services.ProcessingData;
+
+import static org.junit.Assert.*;
 public class senderTest {
     public static void main(String[] args){
-        //sender.readDataLineByLine("C:\\Users\\320091455\\BootCamp\\CaseStudy\\review-case-s3b6\\sample-review\\review-report.csv");
-       //com.philips.properties.client.sender.readDataBySpecificColoumn("C:\\Users\\320091455\\BootCamp\\CaseStudy\\review-case-s3b6\\sample-review\\review-report.csv","Comments");
-       // com.philips.properties.client.sender.readDataBySpecificColoumn("sample-review/review-report.csv","ReviewDate");
-        //reciever.sendWordCountTotheCsvFile();
+        assertTrue(ProcessingData.isNumber("4"));
+        assertFalse(ProcessingData.isNumber("a"));
+        assertFalse(ProcessingData.isNumber("@"));
 
+        assertTrue(ProcessingData.isDate("4/31/2020"));
+        assertFalse(ProcessingData.isDate("13/31/2020"));
+        assertTrue(ProcessingData.isDate("11.11.2020"));
+        assertFalse(ProcessingData.isDate("a.b.c"));
+
+        assertTrue(ProcessingData.isStopWord("the"));
+        assertFalse(ProcessingData.isNumber("name"));
+        assertFalse(ProcessingData.isNumber("name"));
     }
 }

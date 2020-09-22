7141db97976932bd10a9a81ee1500fb60bb46886
harisht26698 <harish.t@philips.com>
2020-09-22 11:02:45 +0530

    sender is modified

--- Sender/src/main/java/com/philips/client/sender.java
+++ Sender/src/main/java/com/philips/client/sender.java
@@ -6,10 +6,18 @@ import java.io.IOException;
 
 public class sender {
 	public static void main(String[] args){
+	    if(args.length==0)
         try {
             PrintingData.bufferDataIsPrintingToConsole();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+        else{
+            try {
+                PrintingData.specificColoumnOfBufferDataPrintedToConsole(args[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
